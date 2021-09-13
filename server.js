fb003fe3df2a1cc08e7769749ce8fbef25874813
ritikagg <mail.ritik.agg@gmail.com>
2021-09-13 22:24:39 +0530

    testing heroku setup

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.get("/", (req, res) => {
-  res.send("API is running on heroku....");
+  res.send("Welocme to localdekho. API is running on heroku....");
 });
 
 app.use(errorMiddleware.notFound);
