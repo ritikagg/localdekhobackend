85f8c7e1fb92e2aa652d8d3fd7ad9842db105ced
ritikagg <mail.ritik.agg@gmail.com>
2021-09-13 22:19:38 +0530

    Changing heroku

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.get("/", (req, res) => {
-  res.send("API is running....");
+  res.send("API is running on heroku....");
 });
 
 app.use(errorMiddleware.notFound);
